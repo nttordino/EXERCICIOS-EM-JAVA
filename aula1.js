f33db2ba6cade71b4e846cb14d42a42a449589c2
Nicolas <tordinonicolas@gmail.com>
2026-02-10 15:36:18 -0300

    EXERCICIOS EM JAVA

---
+++ aula1.js
@@ -0,0 +1,162 @@
+//exercicio 1
+
+const usuario = {
+  login: "admin",
+  senha: "1234",
+  tipo: "administrador"
+};
+
+console.log(`O login do administrador é ${usuario.login} e a senha é ${usuario.senha}, muito fraca por sinal.`);
+
+//exercicio 2
+let aluno = "Lucas"
+let curso = "Técnico em desenvolvimento de sistemas"
+let turma = "DS I3 SESI"
+
+
+const cadastro = {
+  aluno: "Lucas",
+  curso: "Técnico em desenvolvimento de sistemas",
+  turma: "DS I3 SESI"
+};
+
+console.log(`O aluno  ${cadastro.aluno} esta matriculado no curso de  ${cadastro.curso}, na turma ${cadastro.turma}.`);
+
+//exercicio 3
+
+let nome = "Eduardo"
+let disciplina = "Java-script"
+let turno = "manha"
+
+
+const professor = {
+  nome: "Eduardo",
+  disciplina: "Java-script",
+  turno: "manha"
+};
+
+console.log(`O Professor  ${professor.nome} ministra a disciplina de   ${professor.disciplina}, no turno da  ${professor.turno}.`);
+
+//exercicio 4
+
+let nome1 = " Tech Solutions"
+let area  = " Tecnologia"
+let cidade = " Fortaleza "
+
+
+const empresa = {
+  nome1 : " Tech Solutions",
+  area: " Tecnologia ",
+  cidade: "Fortaleza"
+};
+
+console.log(` A empresa  ${empresa.nome1} atua na area de   ${empresa.area}, e fica em ${empresa.cidade}.`);
+
+//exercicio 5
+
+let marca = "  Dell "
+let processador  = " intel I5 "
+let memoria  = " 8GB "
+
+
+const computador = {
+  marca : "  Dell",
+  processador: " intel I5 ",
+  memoria : "8GB"
+};
+
+console.log(` O computador da marca  ${computador.marca} possui um processador   ${computador.processador}, e memoria de  ${computador.memoria}.`);
+
+//exercicio 6
+
+
+let titulo = "  JavaScript Básico"
+let autor  = " Ana Silva "
+let paginas  = " 200 paginas "
+
+
+const livro = {
+  titulo : " JavaScript Básico ",
+  autor: " Ana Silva ",
+  paginas : "200 paginas "
+};
+
+console.log(`O Livro ${livro.titulo} escrito por   ${livro.autor}, possui cerca de  ${livro.paginas}.`);
+
+//exercicio 7
+
+let nome3 = " Matrix"
+let genero  = " Ficção "
+let ano  = " 1999 "
+
+
+const filme = {
+  nome3 : " Matrix ",
+  genero: " Ficção ",
+  ano : " 1999 "
+};
+
+console.log(`O Filme  ${filme.nome3} e do gênero de  ${filme.genero}, lancado em  ${filme.ano}.`);
+
+//exercicio 8
+
+let nome4 = " Minecraft "
+let plataforma  = " PC "
+let genero1  = " SandBox "
+
+
+const jogo = {
+  nome4 : " Minecraft ",
+  plataforma     : " PC ",
+  genero1 : " SandBox "
+};
+
+console.log(`O Jogo   ${jogo.nome4} esta rodando na plataforma de   ${jogo.plataforma}, e pertence ao gênero  ${jogo.genero1}.`);
+
+
+//exercicio 9 
+
+let usuario9 = " Eduardo "
+let senha  = " 1234 "
+let nivel  = " Administrativo "
+
+
+const Conta= {
+  usuario9 : " Eduardo ",
+  senha : " 1234 ",
+  nivel : " Administrativo "
+};
+
+console.log(`O usuario  ${Conta.usuario9} esta em um nivel  ${Conta.nivel}, e utiliza a senha ${Conta.senha}, muito fraca por sinal.`);
+
+//exercicio 10
+
+const Escola = {
+  nome5: "EEEP",
+  cidade1: "Fortaleza",
+  turno1: "manha"
+};
+
+console.log(`A escola  ${Escola.nome5} funcionada na  ${Escola.cidade1}, no turno da ${Escola.turno1}.`);
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
